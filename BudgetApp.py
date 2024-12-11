82b31b2bb119faed3b379b7792d04028b11dd36d
shworc <ojadekses@gmail.com>
2024-12-11 08:35:08 +0100

    finally spent_chart formated

--- BudgetApp.py
+++ BudgetApp.py
@@ -1,72 +1,20 @@
-class Category:
-
-    def __init__(self, category):
-        self.category = category
-        self.ledger = []
-    
-    def __str__(self):
-        title = f"{self.category:*^30}\n"
-        items = ""
-        total = 0
-        for item in self.ledger:
-            items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
-            total += item['amount']
-        output = title + items + "Total: " + str(total)
-        return output
-
-    def getTotals(categories): ###Helper method
-        total = 0
-        breakdown = []
-        for category in categories:
-            total += category.get_withdrawls()
-            breakdown.append(category.get_withdrawals())
-        rounded = list(map(lambda x: truncate(x/total), breakdown))
-        return rounded
-
-    def deposit(self, amount, description=""):
-        self.ledger.append({'amount': amount, 'description': description})
-        #print(self.ledger)
-
-    def withdraw(self, amount, description=""):
-        if not self.check_funds(amount):
-            return False
-        else:
-            self.ledger.append({'amount': -amount, 'description': description})
-            return True
-
-    def get_balance(self):
-        balance = 0
-        for b in self.ledger:
-            print(b["amount"])
-            balance += b["amount"]
-        return balance
-        #return sum(amount for amount in self.ledger)
-
-    def transfer(self, amount, other_category):
-        if (self.check_funds(amount)):
-            self.withdraw(amount, f"Transfer to {self.category}")
-            other_category.deposit(amount, "Transfer from " + self.category)
-            return True
-        return False
-
-    def check_funds(self, amount):
-        if(self.get_balance() >= amount):
-            return True
-        return False
+import math
 
 def create_spend_chart(categories):
     res = "Percentage spent by category\n"
     i = 100
     totals = getTotals(categories)
+    
     while i >= 0:
         cat_spaces = " "
         for total in totals:
             if total * 100 >= i:
-                cat_spaces += "o "
+                cat_spaces += "o  "
             else:
-                cat_spaces += "  "
-        res += str(i).rjust(3) + "|" + cat_spaces + ("\n")
+                cat_spaces += "   "
+        res += str(i).rjust(3) + "|" + cat_spaces + ("\n") 
         i -= 10
+
     dashes = "-" + "---" * len(categories)
     names = []
     x_axis = ""
@@ -81,23 +29,87 @@ def create_spend_chart(categories):
             if x >= len(name):
                 nameStr += "   "
             else:
-                nameStr += name[x] + " "
-        if(x != len(maxi)-1 ):
+                nameStr += name[x] + "  "
+        if(x != len(maxi) -1 ):
             nameStr += '\n'
 
         x_axis += nameStr
 
     res += dashes.rjust(len(dashes)+4) + "\n" + x_axis
+    return res
 
-test = Category('Food')
-print("creating object test")
-print(test.deposit(1000, 'pizza'), "deposit to object test")
-print(repr(test.withdraw(10,'pizza')),"withdrawal from object test, pizza")
+def truncate(n):
+    multiplier = 10
+    return int(n * multiplier) / multiplier
+
+def getTotals(categories): ###Helper method
+    total = 0
+    breakdown = []
+    for category in categories:
+        total += category.get_withdrawls()
+        breakdown.append(category.get_withdrawls())
+    rounded=list(map(lambda x: truncate(x/total), breakdown))
+    return rounded
+
+class Category:
+
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+    
+    def __str__(self):
+        title = f"{self.name:*^30}\n"
+        items = ""
+        total = 0
+        for item in self.ledger:
+            items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
+            total += item['amount']
+        output = title + items + "Total: " + str(total)
+        return output
+
+    def deposit(self, amount, description=""):
+        self.ledger.append({'amount': amount, 'description': description})
+
+    def withdraw(self, amount, description=""):
+        if(self.check_funds(amount)):
+            self.ledger.append({'amount': -amount, 'description': description})
+            return True
+        return False
+
+    def get_balance(self):
+        balance = 0
+        for b in self.ledger:
+            balance += b["amount"]
+        
+        return balance
+
+    def transfer(self, amount, category):
+        if(self.check_funds(amount)):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, "Transfer from " + self.name)
+            return True
+        return False
+
+    def check_funds(self, amount):
+        if(self.get_balance() >= amount):
+            return True
+        return False
+
+    def get_withdrawls(self):
+        total = 0
+        for item in self.ledger:
+            if item["amount"] < 0:
+                total += item["amount"]
+        return total
+
+
+
+
+food = Category('Food')
+food.deposit(1000, 'deposit')
+food.withdraw(10.15, 'groceries')
+food.withdraw(15.89, 'restaurant and more food for dessert')
 clothing = Category('Clothing')
-print("creating object clothing")
-#test.transfer(test.get_balance(), test)
-print((test.transfer(200, clothing))) ###f"transfering from object test to {clothing}"))
-print(test.get_balance(), "test balance")
-print(test)
-print(clothing)
-#create_spend_chart(test)
+food.transfer(50, clothing)
+print(food)
+#print(create_spend_chart(food))
